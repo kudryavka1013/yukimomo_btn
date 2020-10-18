872349bb3ca0c98a3f26a4be48c43726890d8bff
Kudryavka <50348668+kudryavka1013@users.noreply.github.com>
2020-10-18 11:24:14 +0800

    啊

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,40 @@
 <template>
   <v-app>
-    <div>
-    <v-app-bar clipped-left> 
-      <v-app-bar-nav-icon @click="drawer = !drawer" ></v-app-bar-nav-icon>
-      <v-toolbar-title>桃按钮</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" absolute>
+    
+<v-navigation-drawer v-model="drawer" app clipped>
       <v-list>
         <v-list-item-group>
           <v-list-item>
             <v-list-item-icon>
-              <v-icon> </v-icon>
+              <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
             <v-list-item-title>主页</v-list-item-title>
           </v-list-item>
 
           <v-list-item>
             <v-list-item-icon>
-              <v-icon> </v-icon>
+              <v-icon>mdi-web</v-icon>
             </v-list-item-icon>
             <v-list-item-title>哔哩哔哩</v-list-item-title>
           </v-list-item>
 
           <v-list-item>
             <v-list-item-icon>
-              <v-icon> </v-icon>
+              <v-icon>mdi-code-tags</v-icon>
             </v-list-item-icon>
             <v-list-item-title>关于</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-  </div>
+
+   <v-app-bar clipped-left > 
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" ></v-app-bar-nav-icon>
+      <v-toolbar-title>桃按钮</v-toolbar-title>
+    </v-app-bar> 
+   
+    
+  <test/>
     <v-main>
       <Home/>
     </v-main>
@@ -41,11 +43,13 @@
 
 <script>
 import Home from './views/Home'
+import test from './components/test'
 export default {
   name: 'App',
 
   components: {
-    Home
+    Home,
+    test
   },
 
   data: () => ({
