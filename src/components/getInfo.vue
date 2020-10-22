4a2ee958f9277355033e59380909a4eba13b4a3e
Kudryavka <50348668+kudryavka1013@users.noreply.github.com>
2020-10-22 16:12:35 +0800

    Update getInfo.vue
    只能获取直播间开播信息，没有标题和图片，QAQ

--- src/components/getInfo.vue
+++ src/components/getInfo.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <div>阿桃粉丝数：{{ follower }}</div>
-    <div>直播间信息：{{ data }}</div>
+    <div>直播间信息：{{ liveRoomisOpen? "开播啦，好耶！":"啊，没播呢" }}</div>
     <v-btn @click="getFollower">获取粉丝数</v-btn>
-    <v-btn @click="getLiveRoom">获取直播间信息</v-btn>
+    <v-btn @click="getLiveRoomInfo">获取直播间信息</v-btn>
   </div>
 </template>
 
 <script>
 export default {
   name: "getInfo",
-  data: () => ({ follower: "" }),
+  data: () => ({ follower: "有几个粉丝呢？", liveRoomisOpen: 0, }),
+  created: function () {
+    this.getFollower();
+    this.getLiveRoomInfo();
+  },
   methods: {
     getFollower() {
       // .get("https://api.bilibili.com/x/relation/stat?vmid=451793")
@@ -26,14 +30,24 @@ export default {
         )
       );
     },
-    getLiveRoom() {
+    getLiveRoomInfo() {
       // .get("https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid=451793")
       this.$axios({
         url:
-          "https://bird.ioliu.cn/v2?url=https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld",
+          // 不知道为什么远程调api返回的是个html，本来可以拿到更多直播间信息的，QAQ
+          // "https://bird.ioliu.cn/v2?url=https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld",
+          "https://bird.ioliu.cn/v1?url=https://api.live.bilibili.com/room/v1/Room/room_init",
         method: "get",
-        params: { mid: 451793 },
-      }).then((response) => (console.log(response.data)));
+        params: {
+          // mid: 451793,
+          id: 6580433,
+        },
+      }).then(
+        (response) => (
+          (this.liveRoomisOpen = response.data.data.live_status),
+          console.log(response.data)
+        )
+      )
     },
   },
   mounted() {},
