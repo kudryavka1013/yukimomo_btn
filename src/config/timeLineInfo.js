73a7ee03cab9cca3fdd559a748bb173726f375d3
kudryavka1013 <50348668+kudryavka1013@users.noreply.github.com>
2021-03-17 10:11:37 +0800

    时间线完善内容，关于页面完善内容

--- src/config/timeLineInfo.js
+++ src/config/timeLineInfo.js
@@ -10,56 +10,44 @@ const timeLineInfo = [{
     link:'https://www.bilibili.com/video/BV1E541147CC'
 }, {
     color: 'primary',
-    date: '2020.5.5',
-    title: '阿巴阿巴',
-    subtitle:'梦开始的地方',
-    text: '闪亮登场',
+    date: '2020.7.25',
+    title: '万粉纪念回',
+    subtitle:'',
+    text: '',
     icon: 'mdi-star',
     btn: true,
+    btnText:'相关链接',
+    link:'https://www.bilibili.com/video/BV1Xi4y137kt',
+}, {
+    color: 'primary',
+    date: '2020.12.20',
+    title: '生日纪念回',
+    subtitle:'阿巴阿巴',
+    text: '阿巴阿巴',
+    icon: 'mdi-star',
+    btn: false,
+    btnText:'自我介绍',
+    link:''
+}, {
+    color: 'primary',
+    date: '2021.2.10',
+    title: '新衣纪念回',
+    subtitle:'阿巴阿巴',
+    text: '阿巴阿巴',
+    icon: 'mdi-star',
+    btn: false,
     btnText:'自我介绍',
     link:'https://www.bilibili.com/video/BV1E541147CC',
 }, {
     color: 'primary',
-    date: '2020.5.5',
-    title: '阿巴阿巴',
+    date: '2021.2.19',
+    title: '百人舰队达成',
     subtitle:'阿巴阿巴',
     text: '阿巴阿巴',
     icon: 'mdi-star',
     btn: true,
-    btnText:'自我介绍',
-    link:'https://www.bilibili.com/video/BV1E541147CC'
-}, {
-    date: '20.1.4',
-    title: '阿巴阿巴',
-    content: 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent',
-}, {
-    date: '20.1.1',
-    title: 'title',
-    content: 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent',
-}, {
-    date: '20.1.1',
-    title: 'title',
-    content: 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent',
-}, {
-    date: '20.1.1',
-    title: 'title',
-    content: 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent',
-}, {
-    date: '20.1.1',
-    title: 'title',
-    content: 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent',
-}, {
-    date: '20.1.1',
-    title: 'title',
-    content: 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent',
-}, {
-    date: '20.1.1',
-    title: 'title',
-    text: 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent',
-}, {
-    date: '20.1.1',
-    title: 'title',
-    text: 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent',
-}]
+    btnText:'相关链接',
+    link:'https://t.bilibili.com/115691331?type=2',
+}, ]
 
 export default timeLineInfo
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div>
     <span class="title">关于</span>
     <span
-      >这个网站由<a href="https://space.bilibili.com/22735925" target="_blank"
+      >这个网站由 <a href="https://space.bilibili.com/22735925" target="_blank"
         >_kudryavka </a
       >（
       <a href="https://github.com/kudryavka1013" target="_blank">
@@ -10,12 +10,25 @@
       </a>
       ）全力开发并支持
     </span>
-    <span class="title">技术参考</span>
-    <span>前端框架：Vue（2.6.12）</span>
-    <span>前端路由：Vue-Router（3.5.11）</span>
-    <span>前端状态管理：VueX（3.6.2）</span>
-    <span>前端UI组件库：Vuetify（2.3.14）</span>
-    <span>脚手架：Vue-Cli</span>
+    <span class="title mt-3">技术参考</span>
+    <div>
+      <span>前端框架：<a href="https://cn.vuejs.org/" target="_blank" style="color: #63b587">Vue（2.6.12）</a></span>
+      <span>前端路由：<a href="https://router.vuejs.org/zh/" target="_blank" style="color: #63b587">Vue-Router（3.5.11）</a></span>
+      <span>前端状态管理：<a href="https://vuex.vuejs.org/zh/" target="_blank" style="color: #63b587">VueX（3.6.2）</a></span>
+      <span>前端UI组件库：<a href="https://vuetifyjs.com/zh-Hans/" target="_blank" style="color: #4197ef">Vuetify（2.3.14）</a></span>
+      <span>前端脚手架：<a href="https://cli.vuejs.org/zh/" target="_blank" style="color: #63b587">Vue-Cli（4.5.7）</a></span>
+      <span>语言：<a href="https://www.javascript.com/" target="_blank" style="color: #f8db4a">Javascript</a></span>
+      <span>图标库：<a href="https://materialdesignicons.com/" target="_blank" style="color: #4496ec">Material Design Icons</a></span>
+    </div>
+    <span class="title mt-3">项目参考</span>
+    <div>
+      <a style="color: #f4b2a9" href="https://github.com/CaoMeiYouRen/shirakami-haruka-button" target="_blank">豹按钮 (:3っ)∋</a>
+    </div>
+    <span class="title mt-3">友情链接</span>
+    <div>
+      <a style="color: #f4b2a9" href="https://haruka.fun">豹按钮 (:3っ)∋
+      </a>
+    </div>
   </div>
 </template>
 
