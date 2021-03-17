b2ac4fb2f25bebcaf93cfdbd0d48145ac42e8327
kudryavka1013 <50348668+kudryavka1013@users.noreply.github.com>
2021-03-17 08:43:03 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,38 @@
 <template>
-  <div class=""> About </div>
+  <div>
+    <span class="title">关于</span>
+    <span
+      >这个网站由<a href="https://space.bilibili.com/22735925" target="_blank"
+        >_kudryavka </a
+      >（
+      <a href="https://github.com/kudryavka1013" target="_blank">
+        <v-icon>mdi-github</v-icon>kudryavka1013
+      </a>
+      ）全力开发并支持
+    </span>
+    <span class="title">技术参考</span>
+    <span>前端框架：Vue（2.6.12）</span>
+    <span>前端路由：Vue-Router（3.5.11）</span>
+    <span>前端状态管理：VueX（3.6.2）</span>
+    <span>前端UI组件库：Vuetify（2.3.14）</span>
+    <span>脚手架：Vue-Cli</span>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'About'
-}
+  name: "About",
+};
 </script>
 
-<style>
-
+<style scoped>
+div {
+  text-align: center;
+}
+span {
+  display: block;
+}
+a:link {
+  text-decoration: none;
+}
 </style>
